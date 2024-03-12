764057b00d7fa659c72dfc97f7b102b12019ea8a
massire <massire.org@gmail.com>
2024-03-12 13:57:50 +0100

    first commit

---
+++ src/main/java/com/relation/jpa/RelationshipJpaApplication.java
@@ -0,0 +1,13 @@
+package com.relation.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationshipJpaApplication.class, args);
+	}
+
+}
